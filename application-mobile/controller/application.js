7189a6e168ece93de553c9d771f753b2e0e032fc
Adrian <adrian.paul.carrieres@gmail.com>
2019-10-18 11:14:06 -0400

    echaffaud dans application.js

---
+++ application-mobile/controller/application.js
@@ -0,0 +1,8 @@
+var valeursActuelles = [5, 6];
+var comparaison = "La journee est froide et peu lumineuse";
+var tabAujourdHui = [0, 5, 10];
+var tabHabitude = [-20, 4, 30];
+
+var vueValeur = new vueValeur(valeursActuelles, comparaison, tabAujourdHui, tabHabitude);
+
+vueValeur.afficher();
