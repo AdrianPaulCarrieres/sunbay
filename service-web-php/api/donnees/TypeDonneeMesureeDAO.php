95add4dd925d5b21ec859486607f80e26d61df6a
loicbtd <loic.bert.marcel@gmail.com>
2019-10-21 20:05:33 -0400

    écriture de la classe TypeDonneeMesureeDAO

--- service-web-php/api/donnees/TypeDonneeMesureeDAO.php
+++ service-web-php/api/donnees/TypeDonneeMesureeDAO.php
@@ -2,5 +2,40 @@
 
 class TypeDonneeMesureeDAO implements TypeDonneeMesureeSQL
 {
+    private static $instance;
 
+    private $connection;
+    private $listeTypeDonneeMesuree;
+
+    public static function getInstance()
+    {
+        if(is_null(self::$instance)) {
+            self::$instance = new BaseDeDonnees();
+        }
+        return self::$instance;
+    }
+
+    public function __construct()
+    {
+        $this->connection = BaseDeDonnees::getInstance()->getConnection();
+        $this->listeTypeDonneeMesuree = array();
+    }
+
+    public function recupererListeTypeDonneeMesuree()
+    {
+        $curseur = $this->connection->query(TypeDonneeMesureeSQL::SQL_LISTER_TYPE_DONNEE_MESUREE);
+        $this->listeTypeDonneeMesuree = array();
+
+        foreach ($curseur as $item) {
+            $idTypeDonneeMesuree = $item[TypeDonneeMesuree::CLE_ID_TYPE_DONNEE_MESUREE];
+            $etiquette = $item[TypeDonneeMesuree::CLE_ETIQUETTE];
+
+            $typeDonneeMesuree = new TypeDonneeMesuree(
+                $idTypeDonneeMesuree,
+                $etiquette
+            );
+            array_push($this->listeTypeDonneeMesuree, $typeDonneeMesuree);
+        }
+        return $this->listeTypeDonneeMesuree;
+    }
 }
