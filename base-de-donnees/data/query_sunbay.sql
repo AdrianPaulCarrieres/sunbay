7bac4c2c4a1695bd3d7ce39e6fd22c201e4ff311
loicbtd <loic.bert.marcel@gmail.com>
2019-10-22 20:14:27 -0400

    base-de-données : ajout de requêtes

--- base-de-donnees/data/query_sunbay.sql
+++ base-de-donnees/data/query_sunbay.sql
@@ -1,9 +1,47 @@
-SELECT id_type_donnee_mesuree, etiquette FROM type_donnee_mesuree;
+-- LISTER_TYPE_DONNEE_MESUREE
+SELECT id_type_donnee_mesuree, etiquette, unite FROM type_donnee_mesuree;
 
-SELECT id_donnee_mesuree, valeur, instant, id_type_donnee_mesuree FROM donnee_mesuree;
+-- TROUVER_TYPE_DONNEE_MESUREE_PAR_ID
+SELECT id_type_donnee_mesuree, etiquette, unite FROM type_donnee_mesuree WHERE id_type_donnee_mesuree = 1;
 
-SELECT id_calcul, id_type_donnee_mesuree, id_unite, valeur, etiquette, date_debut, date_fin 
-            FROM calcul
-            WHERE id_type_donnee_mesuree = 1
-                AND date_trunc('day', date_debut) = date_trunc('day', now())
-                AND date_trunc('day', date_fin) = date_trunc('day', now());
+-- LISTER_TEMPERATURE_JOUR
+SELECT id_donnee_mesuree, valeur, to_char(instant, 'dd-mm-YYYY HH24:MM:SS') as instant, id_type_donnee_mesuree 
+FROM donnee_mesuree
+WHERE id_type_donnee_mesuree = 1
+    AND date_trunc('day', instant) = date_trunc('day', now())
+ORDER BY instant;
+
+-- LISTER_TEMPERATURE_MOIS
+SELECT id_donnee_mesuree, valeur, to_char(instant, 'dd-mm-YYYY HH24:MM:SS') as instant, id_type_donnee_mesuree 
+FROM donnee_mesuree
+WHERE id_type_donnee_mesuree = 1
+    AND date_trunc('month', instant) = date_trunc('month', now())
+ORDER BY instant;
+
+-- LISTER_TEMPERATURE_ANNEE
+SELECT id_donnee_mesuree, valeur, to_char(instant, 'dd-mm-YYYY HH24:MM:SS') as instant, id_type_donnee_mesuree 
+FROM donnee_mesuree
+WHERE id_type_donnee_mesuree = 1
+    AND date_trunc('year', instant) = date_trunc('year', now())
+ORDER BY instant;
+
+-- LISTER_LUMINOSITE_JOUR
+SELECT id_donnee_mesuree, valeur, to_char(instant, 'dd-mm-YYYY HH24:MM:SS') as instant, id_type_donnee_mesuree 
+FROM donnee_mesuree
+WHERE id_type_donnee_mesuree = 2
+    AND date_trunc('day', instant) = date_trunc('day', now())
+ORDER BY instant;
+
+-- LISTER_LUMINOSITE_MOIS
+SELECT id_donnee_mesuree, valeur, to_char(instant, 'dd-mm-YYYY HH24:MM:SS') as instant, id_type_donnee_mesuree 
+FROM donnee_mesuree
+WHERE id_type_donnee_mesuree = 2
+    AND date_trunc('month', instant) = date_trunc('month', now())
+ORDER BY instant;
+
+-- LISTER_LUMINOSITE_ANNEE
+SELECT id_donnee_mesuree, valeur, to_char(instant, 'dd-mm-YYYY HH24:MM:SS') as instant, id_type_donnee_mesuree 
+FROM donnee_mesuree
+WHERE id_type_donnee_mesuree = 2
+    AND date_trunc('year', instant) = date_trunc('year', now())
+ORDER BY instant;
