955eb43f14a4efe7d4b821aa3689eb784897ea21
Christophe Lachance <lachance.christophe@cgmatane.qc.ca>
2019-10-24 18:36:32 -0400

    Corrections dans le DAO

--- service-web-nodejs/api/DonneeDAO.js
+++ service-web-nodejs/api/DonneeDAO.js
@@ -2,40 +2,46 @@ const {Client} = require('pg');
 var connexion = {
 	  user: 'master', password: '123qweQWE',
 	  host: '192.168.56.10', port: 5432,
-	  database: 'sunbay',};
+	  database: 'sunbay'};
 
 exports.enregistrerDonnee = async function(donnee)
 {
 	var basededonnees = new Client(connexion);
 	await basededonnees.connect();
     
-    const query = {
-        // give the query a unique name
+    const INSERT_TEMPERATURE = {
         name: 'enregistrerDonnee',
         text: 'INSERT INTO DONNEE (valeur, instant, id_type_donnee_mesuree) VALUES $1, $2, $3',
         values: [donnee.temperature, donnee.instant, 1],
       }
-      const query2 = {
-        // give the query a unique name
+      const INSERT_LUMINOSITE = {
         name: 'enregistrerDonnee',
         text: 'INSERT INTO DONNEE (valeur, instant, id_type_donnee_mesuree) VALUES $1, $2, $3',
         values: [donnee.luminosite, donnee.instant, 2],
       }
       // callback
-      basededonnees.query(query, (err, res) => {
+      basededonnees.query(INSERT_TEMPERATURE, (err, res) => {
         if (err) {
           console.log(err.stack)
         } else {
           console.log(res.rows[0])
         }
       })
+      basededonnees.query(INSERT_LUMINOSITE, (err, res) => {
+        if (err) {
+          console.log(err.stack)
+        } else {
+          console.log(res.rows[0])
+        }
+      })
+      
       // promise
       basededonnees
-        .query(query)
+        .query(INSERT_TEMPERATURE)
         .then(res => console.log(res.rows[0]))
         .catch(e => console.error(e.stack));
       basededonnees
-        .query(query2)
+        .query(INSERT_LUMINOSITE)
         .then(res => console.log(res.rows[0]))
         .catch(e => console.error(e.stack))
 
