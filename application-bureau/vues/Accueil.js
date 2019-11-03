f4507635c6f7e10733375740710783068a754f75
Louis Pascuzzi <pascuzzilouis@gmail.com>
2019-11-03 12:26:25 -0500

    mise du header dans une balise script

--- application-bureau/vues/Accueil.js
+++ application-bureau/vues/Accueil.js
@@ -1,33 +1,16 @@
 var Accueil = (function () {
     pageAccueil = document.getElementById("page_accueil").innerHTML;
+    header = document.getElementById("script_header").innerHTML;
 
     return function () {
 
         this.afficher = function () {
             elementBody = document.getElementsByTagName("body")[0];
-            elementBody.innerHTML = pageAccueil;
+            elementBody.innerHTML = header + pageAccueil;
 
-            var ctx = document.getElementById('chartTemp').getContext('2d');
-            var chart = new Chart(ctx, {
-                // The type of chart we want to create
-                type: 'line',
-
-                // The data for our dataset
-                data: {
-                    labels: ['10h', '11h', '12h', '13h', '14h', '17h', '18h'],
-                    datasets: [{
-                        label: 'Température',
-                        backgroundColor: 'rgb(255, 99, 132)',
-                        borderColor: 'rgb(255, 99, 132)',
-                        data: [0, 10, 5, 2, 20, 30, 42]
-                    }]
-                },
-
-                // Configuration options go here
-                options: {}
-            });
 
             var ctx = document.getElementById('chartLum').getContext('2d');
+            ctx.height = 500;
             var chart = new Chart(ctx, {
                 // The type of chart we want to create
                 type: 'line',
@@ -44,7 +27,9 @@ var Accueil = (function () {
                 },
 
                 // Configuration options go here
-                options: {}
+                options: {
+                    maintainAspectRatio: false,
+                }
             });
         }
     }
