3076d915d720f80fe5390084bfede6edb7ebb566
loicbtd <loic.bert.marcel@gmail.com>
2019-11-05 16:08:50 -0500

    le web service mock est fonctionnel

--- service-web-php/api/index.php
+++ service-web-php/api/index.php
@@ -2,15 +2,10 @@
 
 require_once('required.php');
 
-if ( isset($_GET['typeDonnee']) ) $typeDonnee = htmlentities($_GET['typeDonnee']);
 if ( isset($_GET['plage']) ) $plage = htmlentities($_GET['plage']);
 
-if ( !(isset($typeDonnee) || isset($plage)) ) exit();
+if ( !isset($plage) ) exit();
 
-if ( !($typeDonnee == 'luminosite' || $typeDonnee == 'temperature' ) ) exit();
 if ( !($plage == 'jour' || $plage == 'mois' || $plage == 'annee') ) exit();
 
-$calculDAO = CalculDAO::getInstance();
-$calculDAO->recupererListeCalculParTypeDonneeEtPlage($typeDonnee, $plage);
-
-echo $calculDAO->exporter();
+echo CalculateurMock::recupererDonnees($plage);
