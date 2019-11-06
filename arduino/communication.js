36459330652c96a422600c44d287918576c30a69
EstevanGAY <estevan.gay@gmail.com>
2019-11-06 16:03:17 -0500

    Lecture des données depuis le port série + Ajout d'un .md

--- arduino/communication.js
+++ arduino/communication.js
@@ -1,9 +1,16 @@
 const SerialPort = require('serialport');
 const Readline = require('@serialport/parser-readline');
-const port = new SerialPort("COM3", { baudRate: 9600 });
 
-const parser = new Readline();
-port.pipe(parser);
+var  portName = process.argv[2];
 
-parser.on('data', line => console.log(`> ${line}`));
-port.write('ROBOT POWER ON\n');
+const port = new SerialPort(portName, { baudRate: 9600 });
+const parser = port.pipe(new Readline({ delimiter: '\n' }));
+
+// Read the port data
+port.on("open", () => {
+    console.log('serial port open');
+});
+parser.on('data', data => {
+    console.log('got word from arduino:', data);
+});
+
