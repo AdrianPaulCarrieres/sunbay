92b79285cb077c61cf02cca0a6388a348bec4329
Christophe Lachance <lachance.christophe@cgmatane.qc.ca>
2019-12-17 16:26:18 -0500

    Ménage des fichiers qui ne devraient pas être dans cette branche

--- service-web-php/secure/env.developpement.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('BDD_hote','vpsloic.loicbertrand.net'); 
-define('BDD_port','2232'); 
-define('BDD_base_de_donnees','sunbay'); 
-define('BDD_usager','sunbay'); 
-define('BDD_mot_de_passe','Motivation*'); 
