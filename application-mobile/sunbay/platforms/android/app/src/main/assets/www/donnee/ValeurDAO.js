dab312c1ecce75ee41d923f6a118a28b74182f88
AdrianPaulCarrieres <adrian.paul.carrieres@gmail.com>
2019-12-20 05:10:44 -0500

    mobile

---
+++ application-mobile/sunbay/platforms/android/app/src/main/assets/www/donnee/ValeurDAO.js
@@ -0,0 +1,12 @@
+var ValeurDAO = function(){
+    this.lister = function(){
+        
+    }
+
+
+    this.chercher = function(id){
+        console.log("chercher "+ listevaleur[id].nom);
+        
+        return listevaleur[id];
+    }
+}
